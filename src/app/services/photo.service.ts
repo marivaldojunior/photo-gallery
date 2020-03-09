07fc497ad69f2d54069c8b476d87cd6585ca289d
marivaldojunior <marivaldojunior235@gmail.com>
2020-03-09 13:54:18 -0300

    atualizações

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -20,10 +20,45 @@ export class PhotoService {
       source: CameraSource.Camera,
       quality: 100
     });
-    this.photos.unshift({
-      filepath: "soon...",
-      webviewPath: capturedPhoto.webPath
-    });
+    // Save the picture and add it to photo collection
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+  }
+  private async savePicture(cameraPhoto: CameraPhoto) {
+    // Convert photo to base64 format, required by Filesystem API to save
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    // Write the file to the data directory
+    const fileName = new Date().getTime() + '.jpeg';
+    await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+  });
+
+  // Get platform-specific photo filepaths
+  return await this.getPhotoFile(cameraPhoto, fileName);
+  }
+  private async readAsBase64(cameraPhoto: CameraPhoto) {
+    // Fetch the photo, read as a blob, then convert to base64 format
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+    return await this.convertBlobToBase64(blob) as string;
+  }
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
+  
+  private async getPhotoFile(cameraPhoto: CameraPhoto, 
+    fileName: string): Promise<Photo> {
+    return {filepath: fileName,
+    webviewPath: cameraPhoto.webPath
+    };
   }
 
 }
